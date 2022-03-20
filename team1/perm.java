37a1de50dc73874990a56b29c55d06a6f8f5b56b
Ryan <70408766+BossRocket@users.noreply.github.com>
2022-03-20 10:14:10 -0400

    Working solution to permutations problem

--- team1/perm.java
+++ team1/perm.java
@@ -1,98 +1,105 @@
 import java.util.*;
 
-class perm {
-  public static int[] toArray (String num) {
-    int [] arr = new int[num.length()];
-    for (int i = 0; i < num.length(); i++) {
-      arr[i] = Character.getNumericValue(num.charAt(i));
-    }
-    return arr;
-  }
+public class perm
+{
+	public static int[] toArray(String s)
+	{
+		int[] arr = new int[s.length()];
 
-  public static int[] swap (int[] arr, int index) {
-    int swapVal = 9;
-    int swapIndex = 0;
-    for (int i = index + 1; i < arr.length; i++) {
-      if (arr[i] > arr[index] && arr[i] < swapVal) {
-        swapVal = arr[i];
-        swapIndex = i;
-      }
-    }
-    int temp = arr[index];
-    arr[index] = arr[swapIndex];
-    arr[swapIndex] = temp;
-    return arr;
-  }
+		for (int i = 0; i < s.length(); i++)
+		{
+			arr[i] = Character.getNumericValue(s.charAt(i));
+		}
 
-  public static int[] sort (int[] arr, int index) {
-    for (int i = index; i < arr.length; i++) {
-      int lowest = 10;
-      int lowestIndex = i;
-      for (int j = i+1; j < arr.length; j++) {
-        if (arr[j] < lowest && arr[j] < arr[i]) {
-          lowest = arr[j];
-          lowestIndex = j;
-        }
-      }
+		return arr;
+	}
 
-      int temp = arr[i];
-      arr[i] = arr[lowestIndex];
-      arr[lowestIndex] = temp;
+	public static int[] swap(int[] arr, int index)
+	{
+		int lowest = 10;
+		int lowestIndex = -1;
+		for (int i = index; i < arr.length; i++)
+		{
+			if (arr[i] > arr[index] && arr[i] < lowest)
+			{
+				lowest = arr[i];
+				lowestIndex = i;
+			}
+		}
 
-//       for (int j = 0; j < arr.length; j++) {
-//         System.out.print(arr[j]);
-//       }
-//       System.out.println();
-    }
+		int temp = arr[index];
+		arr[index] = arr[lowestIndex];
+		arr[lowestIndex] = temp;
 
-    return arr;
-  }
+		return arr;
+	}
 
-  
-  
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int numCases = input.nextInt();
-    int currCase = input.nextInt();
-    
-    while (currCase != numCases + 1) {
-      String num = input.next();
-      int [] numArr = toArray(num);
-      boolean possible = false;
-      int breakpoint = -1;
-      
-      for (int i = 1; i < numArr.length; i++) {
-        int currNum = numArr[i];
-        int prevNum = numArr[i-1];
-        if (currNum > prevNum) {
-          possible = true;
-          breakpoint = i-1;
-        }
-      }
+	public static int[] sort(int[] arr, int index)
+	{
+		for (int i = index; i < arr.length; i++)
+		{
+			// for (int j = 0; j < arr.length; j++)
+			// {
+			// 	System.out.print(arr[j]);
+			// }
+			// System.out.println();
 
-      if (!possible) {
-        System.out.println(currCase + " BIGGEST");
-        if (!input.hasNextInt()) {
-          break;
-        }
-        currCase = input.nextInt();
-        continue;
-      }
+			int lowestIndex = i;
+			boolean change = false;
+			for (int j = i+1; j < arr.length; j++)
+			{
+				if (arr[j] < arr[i] && arr[j] < arr[lowestIndex])
+				{
+					lowestIndex = j;
+					change = true;
+				}
+			}
 
-      numArr = swap(numArr, breakpoint);
-      numArr = sort(numArr, breakpoint+1);
-      // double newNum = toDouble(numArr);
-      System.out.print(currCase + " ");
-      for (int i = 0; i < numArr.length; i++) {
-        System.out.print(numArr[i]);
-      }
-      System.out.println();
-      if (!input.hasNextInt()) {
-        break;
-      }
-      currCase = input.nextInt();
+			if (change)
+			{
+				int temp = arr[i];
+				arr[i] = arr[lowestIndex];
+				arr[lowestIndex] = temp;
+			}
+		}
 
-    }
-    System.out.println();
-  }
+		return arr;
+	}
+
+	public static void main (String [] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int numCases = input.nextInt();
+		for (int i = 0; i < numCases; i++)
+		{
+			int currCase = input.nextInt();
+			String num = input.next();
+			int[] numArr = toArray(num);
+			int breakpoint = -1;
+			boolean possible = false;
+
+			for (int j = 1; j < numArr.length; j++)
+			{
+				if (numArr[j] > numArr[j-1])
+				{
+					breakpoint = j - 1;
+					possible = true;
+				}
+			}
+
+			if (!possible)
+			{
+				System.out.println(currCase + " BIGGEST");
+				continue;
+			}
+
+			numArr = swap(numArr, breakpoint);
+			numArr = sort(numArr, breakpoint+1);
+
+			System.out.print(currCase + " ");
+			for (int j = 0; j < numArr.length; j++)
+				System.out.print(numArr[j]);
+			System.out.println();
+		}
+	}
 }
