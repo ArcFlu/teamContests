0fef1cac7fd04c1cb95ac658639f0d97aa5fc291
noahhalgren <94983519+noahhalgren@users.noreply.github.com>
2022-04-03 20:37:30 -0400

    update railroad to use dp
    updated railroad to use a dp solution. works for the provided samples but breaks in instances where there are 3 different kinds of cars. that test case is called breakit_railroad.in

--- team3/railroad.java
+++ team3/railroad.java
@@ -1,95 +1,54 @@
-import java.net.Inet4Address;
 import java.util.*;
 
 public class railroad {
-    public static void main(String[] args) {
-        Scanner scnr = new Scanner (System.in);
-        LinkedList<trainYard> q = new LinkedList<trainYard>();
+  public static void main(String[] args) {
+    Scanner scnr = new Scanner (System.in);
 
-        int n = scnr.nextInt();
-        int m = scnr.nextInt();
+    int n = scnr.nextInt();
+    int m = scnr.nextInt();
 
-        while (n != 0 || m != 0) {
+    while (n != 0 || m != 0) {
+      int[] car1 = new int[n];
+      for (int i = 0; i < n; i ++)
+        car1[i] = scnr.nextInt();
 
-            LinkedList<Integer> c1 = new LinkedList<Integer>();
-            for (int i = 0; i < n; i++) {
-                c1.offer(scnr.nextInt());
-                //System.out.println(c1.peekLast());
-            }
+      int[] car2 = new int[m];
+      for (int i = 0; i < m; i++)
+        car2[i] = scnr.nextInt();
 
-            LinkedList<Integer> c2 = new LinkedList<Integer>();
-            for (int i = 0; i < m; i++) {
-                c2.offer(scnr.nextInt());
-                //System.out.println(c2.peekLast());
-            }
+      int[] wanted = new int[n+m];
+      for (int i = 0; i < m+n; i++)
+        wanted[i] = scnr.nextInt();
 
-            LinkedList<Integer> w = new LinkedList<Integer>();
-            for (int i = 0; i < n+m; i++) {
-                w.offer(scnr.nextInt());
-                //System.out.println(w.peekLast());
-            }
+      boolean[][] dp = new boolean[n+1][m+1];
+      for (int j = 0; j <= m; j++)
+        dp[0][j] = true;
 
-            q.offer(new trainYard(c1, c2, w));
-            boolean didIt = false;
-
-            while (!q.isEmpty()) {
-                trainYard curr = q.pop();
-
-                if (curr.wanted.isEmpty()) {
-                    didIt = true;
-                    break;
-                }
-
-                LinkedList<Integer> tempC1 = new LinkedList<Integer>(curr.car1);
-                LinkedList<Integer> tempC2 = new LinkedList<Integer>(curr.car2);
-                LinkedList<Integer> tempW = new LinkedList<Integer>(curr.wanted);
-                if (tempC1.size() > 0)
-                    tempC1.removeLast();
-                if (tempC2.size() > 0)
-                    tempC2.removeLast();
-                tempW.removeLast();
-
-                // if both cars could be taken, add both possibilites to the queue
-                if (curr.wanted.peekLast() == curr.car1.peekLast() && curr.wanted.peekLast() == curr.car2.peekLast()) {
-                    q.offer(new trainYard(tempC1, curr.car2, tempW));
-                    q.offer(new trainYard(curr.car1, tempC2, tempW));
-                }
-
-                // if only car 1 could be taken, take car 1
-                else if (curr.wanted.peekLast() == curr.car1.peekLast())
-                    q.offer(new trainYard(tempC1, curr.car2, tempW));
-
-                // if only car 2 could be taken, take car 2
-                else if (curr.wanted.peekLast() == curr.car2.peekLast())
-                    q.offer(new trainYard(curr.car1, tempC2, tempW));
-
-                // if neither car can be taken, we just continue to next iteration
-                // we already pop'd this case out of q so if it's empty now then it was a bad route
-                // check other routes. if no other routes then not possible
-            }
-
-            if (didIt)
-                System.out.println("possible");
-            else
-                System.out.println("not possible");
-
-            n = scnr.nextInt();
-            m = scnr.nextInt();
+      for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <=m; j++) {
+          dp[i][j] = (car1[i-1] == wanted[i+j-1] && dp[i-1][j]);
+          //System.out.print((car1[i-1] == wanted[i+j-1]) + "+" + dp[i-1][j] + " ");
         }
+        //System.out.println();
+      }
 
+      // catch edge case
+      if (n == 0) {
+        boolean flag = true;
+        for (int i = 0; i < m; i++)
+          flag = flag && (car2[i] == wanted[i]);
+        dp[n][m] = flag;
+      }
+
+      if (dp[n][m])
+        System.out.println("possible");
+      else
+        System.out.println("not possible");
+
+      n = scnr.nextInt();
+      m = scnr.nextInt();
     }
 
-    static class trainYard {
-        LinkedList<Integer> car1;
-        LinkedList<Integer> car2;
-        LinkedList<Integer> wanted;
-
-        trainYard (LinkedList<Integer> c1, LinkedList<Integer> c2, LinkedList<Integer> w) {
-            car1 = new LinkedList<Integer>(c1);
-            car2 = new LinkedList<Integer>(c2);
-            wanted = new LinkedList<Integer>(w);
-        }
-
-    }
-
+  }
+  
 }
