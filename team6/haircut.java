bba983223c19cf794e034ebd94bc9fba8de6bee1
Ryan <70408766+BossRocket@users.noreply.github.com>
2022-04-25 11:14:32 -0400

    Solution to haircut

--- team6/haircut.java
+++ team6/haircut.java
@@ -1,80 +1,48 @@
 import java.util.*;
 
 public class haircut {
-    public static int findBarber (int numBarbers, int placeInLine, double[] barbers) {
-        if (numBarbers >= placeInLine)
-            return placeInLine;
+	public static long findBarber(long[] barbers, long spot) {
+		long high = (long)Integer.MAX_VALUE * 100000;
+		long low = 0;
+		long mid = 0;
+		while (low < high) {
+			mid = (low + high) / 2;
+			long customer = barbers.length;
+			for (int i = 0; i < barbers.length; i++)
+				customer += mid / barbers[i];
 
-        if (numBarbers == 1)
-            return 1;
+			if (customer < spot)
+				low = mid + 1;
+			else
+				high = mid;
+		}
 
-        int currCustomer = numBarbers;
-        double minute = 1;
-        while (true) {
-            for (int j = 0; j < numBarbers; j++) {
-                if (minute % barbers[j] == 0) {
-                    currCustomer++;
-                    if (currCustomer == placeInLine) {
-                        return (j+1);
-                    }
-                }
-            }
-            minute += 1;
-//            System.out.println(minute);
-        }
-    }
+		long customer = barbers.length + 1;
+		long time = low - 1;
+		for (int i = 0; i < barbers.length; i++)
+			customer += time / barbers[i];
 
-    public static int findBarberBetter(int numBarbers, int placeInLine, double[] barbers, double fastestTime) {
-        if (numBarbers >= placeInLine)
-            return placeInLine;
-        if (numBarbers == 1)
-            return 1;
+		time += 1;
+		for (int i = 0; i < barbers.length; i++)
+			if (time % barbers[i] == 0)
+				if (customer++ == spot)
+					return i+1;
 
-        int currCustomer = numBarbers;
-        double round = 1;
-        double time = (double)fastestTime;
-        double[] ogTimes = Arrays.copyOf(barbers, numBarbers);
-        while (true) {
-            for (int i = 0; i < numBarbers; i++) {
-                if (time >= barbers[i]) {
-                    currCustomer++;
-                    if (currCustomer == placeInLine) {
-                        // System.out.println(round);
+		return -1; // no match, only returns if code is flawed
+	}
 
-                        return (i+1);
-                    }
-                    barbers[i] += ogTimes[i];
-                    // System.out.println("Barber " + (i+1) + " update: " + barbers[i]);
-                    // System.out.println("ogTimes " + ogTimes[i]);
-                }
-            }
-            round++;
-            time = (double)fastestTime * round;
-            System.out.println("round: " + round + " || time: " + time);
-        }
-    }
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int numCases = input.nextInt();
+		for (int currCase = 1; currCase <= numCases; currCase++) {
+			int numBarbers = input.nextInt();
+			long spot = input.nextInt();
+			long[] barbers = new long[numBarbers];
+			for (int i = 0; i < numBarbers; i++)
+				barbers[i] = input.nextInt();
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int numCases = input.nextInt();
-        for (int currCase = 1; currCase <= numCases; currCase++) {
-            int numBarbers = input.nextInt();
-            int placeInLine = input.nextInt();
-            double[] barbers = new double[numBarbers];
-            double fastestTime = 1000000;
-            for (int i = 0; i < numBarbers; i++) {
-                barbers[i] = input.nextDouble();
-                if (barbers[i] < fastestTime)
-                    fastestTime = barbers[i];
-                // System.out.println("Barber #" + (i+1) + ": " + barbers[i]);
-            }
-            // System.out.println(fastestTime);
-            // System.out.println(placeInLine);
-
-           // int myBarber = findBarber(numBarbers, placeInLine, barbers);
-            int myBarber = findBarberBetter(numBarbers, placeInLine, barbers, fastestTime);
-            System.out.println("Case #" + currCase + ": " + myBarber);
-            // System.out.println();
-        }
-    }
+			long myBarber = findBarber(barbers, spot);
+			System.out.println("Case #" + currCase + ": " + myBarber);
+		}
+	}
 }
